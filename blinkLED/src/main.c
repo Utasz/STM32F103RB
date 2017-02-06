18adf9dff0d2e1e2bbd3bcd97c1eb00713a8d7d1
Utasz <lsloviq@gmail.com>
2017-02-07 00:18:05 +0100

    Edit comments

--- blinkLED/src/main.c
+++ blinkLED/src/main.c
@@ -1,13 +1,15 @@
+//NUCLEO F103RBT6
+//onboard LED on PA5
+
 #include "stm32f10x.h"
 
 int main(void){
 
-	/* Enable HSI 8MHz */
+	/* Enable internal clock - HSI 8MHz */
 	  RCC->CR |= ((uint32_t)RCC_CR_HSION);
 
 	  /* Wait for HSI to be ready */
 	while ((RCC->CR & RCC_CR_HSIRDY) == 0){
-	      // Nop
 	  }
 
 	  /* Set HSI as the System Clock */
@@ -15,10 +17,9 @@ int main(void){
 
 	  /* Wait for HSI to be used for the system clock */
 	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI){
-	      // Nop
 	  }
 
-	RCC->APB2ENR = RCC_APB2ENR_IOPAEN; //turn clocks on for PORT B
+	RCC->APB2ENR = RCC_APB2ENR_IOPAEN; //turns clock on for PORT A
 
 
 	GPIOA->CRL |= GPIO_CRL_MODE5_1; //push_pull, output, 2MHz max output speed
@@ -26,7 +27,7 @@ int main(void){
 
 
 	SysTick_Config(16000000);
-	//SysTick->CTRL &= ~SysTick_CTRL_CLKSOURCE_Msk; //
+	//interrupt from SysTick, led blinks in 2s intervals
 
 	while(1);
 
